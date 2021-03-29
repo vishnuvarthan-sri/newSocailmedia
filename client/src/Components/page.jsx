64ccd7c8005e2c2fcfc837430a567cdd2121ad83
vishnuvarthan <vishnuvarthan375@gmail.com>
2021-03-29 18:16:11 +0530

    changes

--- client/src/Components/page.jsx
+++ client/src/Components/page.jsx
@@ -100,7 +100,7 @@ class Page extends React.Component{
   }
 
 componentDidUpdate(prevProps){
-  if(this.props.user.posted != prevProps.user.posted){
+  if(prevProps.user.posted != this.props.user.posted){
     this.props.image();
   }
 }
